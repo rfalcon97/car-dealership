6723c2904d668ca1dc315768dbc37cffce638d63
Ricardo Falcon <ricardooldu97@gmail.com>
2025-04-24 22:53:39 -0500

    change

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name,
+      createdAt: new Date().getTime(),
+    };
+    this.brands.push(brand);
+
+    return brand;
+  }
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) {
+      throw new NotFoundException(`Brand with id ${id} not found`);
+    }
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDb = this.findOne(id);
+    this.brands = this.brands.map((brand) => {
+      if (brand.id === id) {
+        brandDb.updatedAt = new Date().getTime();
+        brandDb = {
+          ...brandDb,
+          ...updateBrandDto,
+        };
+        return brandDb;
+      }
+      return brand;
+    });
+  }
+
+  remove(id: string) {
+    this.brands = this.brands.filter((brand) => brand.id !== id);
+  }
+}
